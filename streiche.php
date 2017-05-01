1472447003d8f7e9119bdf86af1b82965495398c
Yves Ledermann <yves@laedis.ch>
2017-05-01 13:31:52 +0200

    add streiche.php (working on hg.bramberg)

---
+++ streiche.php
@@ -0,0 +1,183 @@
+<p>
+<select id="teamSelect" size="3" multiple="multiple"></select>
+<select id="jahrSelect" style="vertical-align: top;"></select>
+<input id="nurMeisterschaftCb" style="margin: 0; vertical-align: top;" type="checkbox" name="meisterschaft"><span style="vertical-align: top;">&nbsp;Nur Meisterschaft</span>
+</p>
+
+<table id="table" style="width: 100%;" border="0" cellspacing="0" cellpadding="0">
+	<colgroup>
+		<col style="width: 77pt;" span="2" width="103">
+		<col style="width: 23pt;" span="10" width="31">
+		<col style="width: 27pt;" span="11" width="36">
+		<col style="width: 27pt;" width="36">
+		<col style="width: 27pt;" span="9" width="36">
+		<col style="width: 40pt;" width="53"> </colgroup>
+	<tbody>
+		<tr id="tableheader" style="height: 13.5pt;">
+			<td style="height: 13.5pt; width: 77pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none solid solid; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; border-left-width: 0.5pt; border-left-color: #95b3d7; background: #4f81bd;" width="103" height="18" data-sorttype="string" data-sort="nachname">Name</td>
+			<td style="width: 77pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="103" data-sorttype="string" data-sort="vorname">Vorname</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">0</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">1</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">2</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">3</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">4</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">5</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">6</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">7</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">8</td>
+			<td style="width: 23pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="31">9</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">10</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">11</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">12</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">13</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">14</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">15</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">16</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">17</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">18</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">19</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">20</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">21</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">22</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">23</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">24</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">25</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">26</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">27</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">28</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">29</td>
+			<td style="width: 27pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="36">30</td>
+			<td style="width: 40pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid solid solid none; border-top-color: #4f81bd; border-right-width: 0.5pt; border-right-color: #95b3d7; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;" width="53" data-sorttype="numeric" data-sort="punkte">Total</td>
+		</tr>
+	</tbody>
+</table>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/qooxdoo/4.1/q.min.js"></script>
+
+<script type="text/javascript">
+	(function() {
+		var club = 'bramberg';
+		var sortColumn, sortDirection, sortType, data;
+		q.ready(function() {
+			var xmlhttp = q.io.xhr("https://www.hgverwaltung.ch/api/1/" + club + "/spiele/jahre");
+			xmlhttp.on('loadend', function(xhr) {
+				if (xhr.responseText) {
+					var jahre = JSON.parse(xhr.responseText);
+					var jahrSelect = q("#jahrSelect");
+					jahre.forEach(function(j) {
+						jahrSelect.append('<option value=' + j + '>' + j + '</option>');
+					});
+					jahrSelect.on("change", getData);
+					q("#nurMeisterschaftCb").on("change", getData);
+					getData();
+				}
+			});
+			xmlhttp.send();
+			var xmlhttpteam = q.io.xhr("https://www.hgverwaltung.ch/api/1/" + club + "/mannschaften");
+			xmlhttpteam.on('loadend', function(xhr) {
+				if (xhr.responseText) {
+					var teams = JSON.parse(xhr.responseText);
+					var teamSelect = q("#teamSelect");
+					teams.forEach(function(j) {
+						teamSelect.append('<option value=' + j + '>' + j + '</option>');
+					});
+					teamSelect.on("change", getData);
+					getData();
+				}
+			});
+			xmlhttpteam.send();
+			q("#tableheader td").on("click", onHeaderClick);
+		});
+		function getData() {
+			var jahr = q("#jahrSelect").getValue();
+			var team = q("#teamSelect").getValue();
+			var nurMeisterschaft = q("#nurMeisterschaftCb").getProperty('checked');
+			if (jahr) {
+				sortColumn = 'nachname';
+				sortDirection = '';
+				sortType = 'string';
+				var alle = nurMeisterschaft ? '0' : '1';
+				var xmlhttp = q.io.xhr("https://www.hgverwaltung.ch/api/1/" + club + "/durchschnitt/" + team + "?sort=nachname&alle=" + alle + "&excludeSpieler=Ersatz&streicheDetail=true&jahr=" + jahr);
+				xmlhttp.on('loadend', function(xhr) {
+					if (xhr.responseText) {
+						data = JSON.parse(xhr.responseText);
+						showData(data);
+					}
+				});
+				xmlhttp.send();
+			}
+		}
+		function showData(datas) {
+			for (var i = 0; i < datas.length; i++) {
+				var data = datas[i];
+				if (data.schnitt && data.schnittVorjahr) {
+					data.diff = parseFloat(data.schnitt) - parseFloat(data.schnittVorjahr);
+				}
+			}
+			datas.sort(createSortFunction());
+			q("#table tr.g").remove();
+			var style = ['height: 12.75pt; width: 77pt; font-size: 10pt; color: #365f91; font-weight: bold; font-family: Calibri; border-top-width: 0.5pt; border-style: solid none solid solid; border-top-color: #95b3d7; border-bottom-width: 0.5pt; border-bottom-color: #95b3d7; border-left-width: 0.5pt; border-left-color: #95b3d7; background: #d3dfee;', 'height: 12.75pt; width: 77pt; font-size: 10pt; color: #365f91; font-weight: bold; font-family: Calibri; border-top-width: 0.5pt; border-style: solid none solid solid; border-top-color: #95b3d7; border-bottom-width: 0.5pt; border-bottom-color: #95b3d7; border-left-width: 0.5pt; border-left-color: #95b3d7;'];
+			var ix = 0;
+			for (var i = 0; i < datas.length; i++) {
+				var data = datas[i];
+				if (data.streiche === 0) {
+					continue;
+				}
+				var si = ix % 2;
+				ix++;
+				var row = '<tr class="g" style="height: 12.75pt;">';
+				row += '<td style="' + style[si] + '">' + data.nachname + '</td>';
+				row += '<td style="' + style[si] + '">' + data.vorname + '</td>';
+				for (var s = 0; s <= 30; s++) {
+					if (data.streicheDetail[s] !== 0) {
+						row += '<td style="' + style[si] + '">' + data.streicheDetail[s] + '</td>';
+					} else {
+						row += '<td style="' + style[si] + '"></td>';
+					}
+				}
+				row += '<td style="' + style[si] + 'text-align:right;padding-right:10px">' + data.streiche + '</td>';
+				row += '</tr>';
+				q("#table > tbody:last").append(row);
+			}
+		}
+		function createSortFunction() {
+			if (sortType === 'string') {
+				return function(a, b) {
+					var x = a[sortColumn].toLowerCase();
+					var y = b[sortColumn].toLowerCase();
+					if (sortDirection !== '-') {
+						return x < y ? -1 : x > y ? 1 : 0;
+					} else {
+						return x < y ? 1 : x > y ? -1 : 0;
+					}
+				}
+			} else {
+				return function(a, b) {
+					if (sortDirection !== '-') {
+						return a[sortColumn] - b[sortColumn];
+					} else {
+						return b[sortColumn] - a[sortColumn];
+					}
+				}
+			}
+		}
+		function onHeaderClick(e) {
+			var target = q(e.getTarget());
+			var sort = target.getData('sort');
+			if (sort) {
+				if (sortColumn === sort) {
+					if (sortDirection === '-') {
+						sortDirection = '';
+					} else {
+						sortDirection = '-';
+					}
+				} else {
+					sortColumn = sort;
+					sortDirection = '';
+					sortType = target.getData('sorttype');
+				}
+			}
+			showData(data);
+		}
+	})();
+</script>
