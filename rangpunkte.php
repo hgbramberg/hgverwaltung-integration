4f4652663c50d74a0bc3d2bb49318e1ce21f80d8
Yves Ledermann <yves@laedis.ch>
2017-05-01 13:24:00 +0200

    add rangpunkte.php (working on hg.bramberg)

---
+++ rangpunkte.php
@@ -0,0 +1,230 @@
+<p>
+    <select id="teamSelect" size="3" multiple="multiple">
+        </select> <select id="jahrSelect" style="vertical-align: top;">
+        </select>
+</p>
+
+<table id="table" border="0" cellspacing="0" cellpadding="0" style="width: 100%;">
+    <colgroup>
+        <col style="width: 101pt;" span="2" width="134" />
+        <col style="width: 44pt;" span="12" width="58" />
+    </colgroup>
+
+    <tbody>
+        <tr style="height: 15.75pt;" id="tableheader">
+            <td style=
+            "height: 15.75pt; width: 101pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none solid solid; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; border-left-width: 0.5pt; border-left-color: #95b3d7; background: #4f81bd;"
+            width="134" height="21" data-sort="nachname" data-sorttype="string">
+                Nachname
+            </td>
+            <td style=
+            "width: 101pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="134" data-sort="vorname" data-sorttype="string">
+                Vorname
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                1
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                2
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                3
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                4
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                5
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                6
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                7
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                8
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                9
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                10
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid none; border-top-color: #4f81bd; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;"
+            width="58">
+                11
+            </td>
+            <td style=
+            "width: 44pt; font-size: 10pt; color: white; font-weight: bold; font-family: Calibri; border-top-width: 1pt; border-style: solid solid solid none; border-top-color: #4f81bd; border-right-width: 0.5pt; border-right-color: #95b3d7; border-bottom-width: 1pt; border-bottom-color: #4f81bd; background: #4f81bd;text-align:right;padding-right:20px"
+            width="58" data-sort="rangpunkte" data-sorttype="numeric">
+                Total
+            </td>
+        </tr>
+    </tbody>
+</table>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/qooxdoo/4.1/q.min.js" type="text/javascript">
+</script><script type="text/javascript">
+//<![CDATA[
+
+ (function() {
+
+    var club = 'bramberg';
+
+    var sortColumn, sortDirection, sortType, data;
+        var style = ['height: 15pt; width: 101pt; font-size: 10pt; color: #365f91; font-weight: bold; font-family: Calibri; border-top-width: 0.5pt; border-style: solid none solid solid; border-top-color: #95b3d7; border-bottom-width: 0.5pt; border-bottom-color: #95b3d7; border-left-width: 0.5pt; border-left-color: #95b3d7; background: #d3dfee;',
+                 'height: 15pt; width: 101pt; font-size: 10pt; color: #365f91; font-weight: bold; font-family: Calibri; border-top-width: 0.5pt; border-style: solid none solid solid; border-top-color: #95b3d7; border-bottom-width: 0.5pt; border-bottom-color: #95b3d7; border-left-width: 0.5pt; border-left-color: #95b3d7;' ];
+
+
+        q.ready(function() {
+                var xmlhttp = q.io.xhr("https://www.hgverwaltung.ch/api/1/"+club+"/spiele/jahre");
+                xmlhttp.on('loadend', function(xhr) {
+                        if (xhr.responseText) {
+                                var jahre = JSON.parse(xhr.responseText);
+                                var jahrSelect = q("#jahrSelect");
+                                jahre.forEach(function(j) {
+                                        jahrSelect.append('<option value='+j+'>'+j+'<\/option>');
+                                });
+
+                                jahrSelect.on("change", getData);
+                                getData();
+                        }
+                });
+                xmlhttp.send();
+
+                var xmlhttpteam = q.io.xhr("https://www.hgverwaltung.ch/api/1/"+club+"/mannschaften");
+                xmlhttpteam.on('loadend', function(xhr) {
+                        if (xhr.responseText) {
+                                var teams = JSON.parse(xhr.responseText);
+                                var teamSelect = q("#teamSelect");
+                                teams.forEach(function(j) {
+                                        teamSelect.append('<option value='+j+'>'+j+'<\/option>');
+                                });
+
+                                teamSelect.on("change", getData);
+                                getData();
+                        }
+                });
+                xmlhttpteam.send();
+                q("#tableheader td").on("click", onHeaderClick);
+        });
+
+        function getData() {
+          var jahr = q("#jahrSelect").getValue();
+          var team = q("#teamSelect").getValue();
+
+          if (jahr) {
+            sortColumn = 'nachname';
+            sortDirection = '';
+            sortType = 'string';
+            var xmlhttp = q.io.xhr("https://www.hgverwaltung.ch/api/1/"+club+"/durchschnitt/"+team+"?alle=0&rangpunkteDetail=true&excludeSpieler=Ersatz&sort=nachname&jahr="+jahr);
+            xmlhttp.on('loadend', function(xhr) {
+                        if (xhr.responseText) {
+                           data = JSON.parse(xhr.responseText);
+                           showData(data);
+                        }
+                });
+            xmlhttp.send();
+          }
+        }
+
+        function showData(datas) {
+                datas.sort(createSortFunction());
+                q("#table tr.g").remove();
+                var ix = 0;
+                for (var i = 0; i < datas.length; i++) {
+                        var data = datas[i];
+                        if (data.streiche === 0) {
+                          continue;
+                        }
+                      var si = ix % 2;
+                     ix++;
+                        var row = '<tr class="g" style="height: 15pt;">';
+                        row += '<td style="'+style[si]+'">'+data.nachname+'<\/td>';
+                        row += '<td style="'+style[si]+'">'+data.vorname+'<\/td>';
+
+                        for (var j = 0; j < 11; j++) {
+                                row += '<td style="'+style[si]+'">';
+                                if (data.rangpunkteSpiele && data.rangpunkteSpiele[j]) {
+                                        row += data.rangpunkteSpiele[j];
+                                }
+                                row += '<\/td>';
+                        }
+
+
+                        row += '<td style="'+style[si]+'text-align:right;padding-right:20px">'+data.rangpunkte+'<\/td><\/tr>';
+
+                        q("#table > tbody:last").append(row);
+                }
+        }
+
+        function createSortFunction() {
+                if (sortType === 'string') {
+                        return function(a, b) {
+                      var x = a[sortColumn].toLowerCase();
+                  var y = b[sortColumn].toLowerCase();
+                  if (sortDirection !== '-') {
+                                return x < y ? -1 : x > y ? 1 : 0;
+                          } else {
+                            return x < y ? 1 : x > y ? -1 : 0;
+                          }
+                        }
+                }
+                else {
+                        return function(a, b) {
+                          if (sortDirection !== '-') {
+                        return a[sortColumn] - b[sortColumn];
+                      } else {
+                        return b[sortColumn] - a[sortColumn];
+                      }
+                        }
+                }
+        }
+
+        function onHeaderClick(e) {
+            var target = q(e.getTarget());
+                var sort = target.getData('sort');
+
+                if (sort) {
+                        if (sortColumn === sort) {
+                           if (sortDirection === '-') {
+                             sortDirection = '';
+                           } else {
+                             sortDirection = '-';
+                           }
+                        } else {
+                           sortColumn = sort;
+                           sortDirection = '';
+                           sortType = target.getData('sorttype');
+                        }
+                }
+
+                showData(data);
+        }
+
+  })();
+
+//]]>
+</script>
